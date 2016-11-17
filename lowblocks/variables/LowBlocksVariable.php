9d9cda4fbd8125e5c2e8b3425a88fcedeeaa9bae
Neil Anderson <neil.anderson@whitespacers.com>
2016-11-17 09:59:12 +0000

    fix indenting

--- lowblocks/variables/LowBlocksVariable.php
+++ lowblocks/variables/LowBlocksVariable.php
@@ -3,22 +3,21 @@ namespace Craft;
 
 class LowBlocksVariable
 {
-
 	protected $criteria;
 
 	public function __construct()
 	{
 		$this->criteria = craft()->elements->getCriteria(ElementType::MatrixBlock);
 	}
-	
-    public function id($id)
-    {
-        $this->criteria->id = $id;
-        return $this->criteria->first();
-    }
 
-    public function blocks()
-    {
-        return $this->criteria;
-    }
+	public function id($id)
+	{
+		$this->criteria->id = $id;
+		return $this->criteria->first();
+	}
+
+	public function blocks()
+	{
+		return $this->criteria;
+	}
 }
